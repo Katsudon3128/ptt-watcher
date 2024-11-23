1e59b3e3678ebe3c9e9bb161d9b27489eb8a5688
CHARLIE-LIN-HOM\charlie-lin <holeinone111111@gmail.com>
2024-11-23 22:37:11 +0800

    調整看板紀錄程式&新增轉檔程式

--- BoardRankLog.py
+++ BoardRankLog.py
@@ -2,11 +2,37 @@ import csv
 import time
 import requests
 from bs4 import BeautifulSoup
+import signal
+import sys
 
-hotpage = requests.get("https://www.ptt.cc/bbs/index.html")
+# 定義中止函數
+def handler(signum, frame):
+    print("程式終止")
+    sys.exit(0)
 
-main = BeautifulSoup(hotpage.text, 'html.parser')
-board_find = main.find_all('a', class_='board', limit=10)
+# 設置 Ctrl+C 觸發中止
+signal.signal(signal.SIGINT, handler)
+print("輸入 Ctrl+C 中止...")
 
-for board in board_find:
-    print(board.find('div', class_ = 'board-name').text + ":" + board.select('span')[0].text)
+try:
+    while True:
+        file = open('board_log.csv', mode='a', newline='')
+        writer = csv.writer(file)
+        hotpage = requests.get("https://www.ptt.cc/bbs/index.html")
+        main = BeautifulSoup(hotpage.text, 'html.parser')
+        board_find = main.find_all('a', class_='board', limit=10)
+        writer.writerow([time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                        ,board_find[0].find('div', class_ = 'board-name').text, board_find[0].select('span')[0].text
+                        ,board_find[1].find('div', class_ = 'board-name').text, board_find[1].select('span')[0].text
+                        ,board_find[2].find('div', class_ = 'board-name').text, board_find[2].select('span')[0].text
+                        ,board_find[3].find('div', class_ = 'board-name').text, board_find[3].select('span')[0].text
+                        ,board_find[4].find('div', class_ = 'board-name').text, board_find[4].select('span')[0].text
+                        ,board_find[5].find('div', class_ = 'board-name').text, board_find[5].select('span')[0].text
+                        ,board_find[6].find('div', class_ = 'board-name').text, board_find[6].select('span')[0].text
+                        ,board_find[7].find('div', class_ = 'board-name').text, board_find[7].select('span')[0].text
+                        ,board_find[8].find('div', class_ = 'board-name').text, board_find[8].select('span')[0].text
+                        ,board_find[9].find('div', class_ = 'board-name').text, board_find[9].select('span')[0].text])
+        file.close()
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
---
+++ DataBind.py
@@ -0,0 +1,51 @@
+import csv
+import signal
+import sys
+import time
+
+# 定義中止函數
+def handler(signum, frame):
+    print("程式終止")
+    sys.exit(0)
+
+# 設置 Ctrl+C 觸發中止
+signal.signal(signal.SIGINT, handler)
+print("輸入 Ctrl+C 中止...")
+
+try:
+    while True:
+        log_file = open('board_log.csv')
+        data_file = open('data.csv',mode='w', newline='')
+
+        reader = csv.reader(log_file)
+        data_list = list(reader)
+
+        writer = csv.DictWriter(data_file, ['LogTime',data_list[len(data_list)-1][1]
+                                                    ,data_list[len(data_list)-1][3]
+                                                    ,data_list[len(data_list)-1][5]
+                                                    ,data_list[len(data_list)-1][7]
+                                                    ,data_list[len(data_list)-1][9]
+                                                    ,data_list[len(data_list)-1][11]
+                                                    ,data_list[len(data_list)-1][13]
+                                                    ,data_list[len(data_list)-1][15]
+                                                    ,data_list[len(data_list)-1][17]
+                                                    ,data_list[len(data_list)-1][19]], restval=0, extrasaction='ignore')
+        writer.writeheader()
+
+        for i in range(len(data_list)-200, len(data_list)):
+            writer.writerow({'LogTime':data_list[i][0], data_list[i][1]:data_list[i][2]
+                                                    , data_list[i][3]:data_list[i][4]
+                                                    , data_list[i][5]:data_list[i][6]
+                                                    , data_list[i][7]:data_list[i][8]
+                                                    , data_list[i][9]:data_list[i][10]
+                                                    , data_list[i][11]:data_list[i][12]
+                                                    , data_list[i][13]:data_list[i][14]
+                                                    , data_list[i][15]:data_list[i][16]
+                                                    , data_list[i][17]:data_list[i][18]
+                                                    , data_list[i][19]:data_list[i][20]})
+
+        log_file.close()
+        data_file.close()
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
